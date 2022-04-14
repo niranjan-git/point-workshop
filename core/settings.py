ae1ddc7edf93dfbc37099e41592e008fdc0f8d4f
niranjan-git <61872255+niranjan-git@users.noreply.github.com>
2022-04-14 14:12:44 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -168,7 +168,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'notify.user.management@gmail.com' # os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'tsbrhqwmngrqikmc' # os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'Password' # os.getenv('EMAIL_HOST_PASSWORD')
 APP_NAME = 'JRF AT DESIDOC'
 
 # mail Configuration ends
