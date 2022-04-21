334f911facddc197d9b1fe346630fda2aecc1ccd
niranjan <kniranjanr@gmail.com>
2022-04-21 17:19:23 +0530

    login required decorator

--- point_workshop/views.py
+++ point_workshop/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 # class Home(LoginRequiredMixin, generic.TemplateView):
 #     template_name = "index.html"
 
-
+@login_required
 def home(request):
     print(request.user.roles.all())
     return render(request, 'index.html')
